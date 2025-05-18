15b6d2b3688007678b2072b1ec99fe244c56d1aa
Lucas Laurindo <caiolucax2024@gmail.com>
2025-05-18 15:34:47 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('btn-enviar').addEventListener("click", () => {
-    const nome = document.getElementById('input-nome');
-    const telefone = document.getElementById('input-telefone').value;
-    const mensagem = document.getElementById('input-mensagem').value;
-    const link = `https://api.whatsapp.com/send?phone=${telefone}&text=${mensagem}`;
-    window.open(link);
-})
-
-
